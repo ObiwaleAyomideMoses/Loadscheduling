974ad01fabb1d1d9e6d3050a85c1300b0c95752d
Obiwale Ayomide Moses <obiwaleayomidemoses@gmail.com>
2024-03-13 06:19:11 +0100

    test

--- App.tsx
+++ App.tsx
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { useState } from 'react';
+import axios from 'axios';
 export default function App() {
+  const [response, setRsponse] = useState('');
+  const request = async () => {
+    const response = await axios.get('http://192.168.4.1/16/on');
+    console.log(response.data);
+    setRsponse(response.data);
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text onPress={() => request()}>
+        Open up App.tsx to start working on your app!
+      </Text>
+      <Text>{response.statePin16}</Text>
       <StatusBar style="auto" />
     </View>
   );
